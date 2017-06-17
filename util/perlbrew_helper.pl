a62911e9409118bab4e152c4bd52f8604faa0620
Jaideep Sundaram <sundaram.medimmune@gmail.com>
2017-06-16 22:56:34 -0400

    initial check-in

---
+++ util/perlbrew_helper.pl
@@ -0,0 +1,303 @@
+#!/usr/bin/env perl
+use strict;
+use Cwd;
+use Carp;
+use Pod::Usage;
+use File::Spec;
+use File::Path;
+use Term::ANSIColor;
+use FindBin;
+use Getopt::Long qw(:config no_ignore_case no_auto_abbrev);
+
+use lib "$FindBin::Bin/../lib";
+
+use DevelopmentUtils::Logger;
+use DevelopmentUtils::Config::Manager;
+use DevelopmentUtils::Perlbrew::Helper;
+
+use constant TRUE => 1;
+
+use constant FALSE => 0;
+
+use constant DEFAULT_CONFIG_FILE => "$FindBin::Bin/../conf/commit_code.ini";
+
+use constant DEFAULT_VERBOSE   => FALSE;
+
+use constant DEFAULT_LOG_LEVEL => 4;
+
+use constant DEFAULT_INDIR => File::Spec->rel2abs(cwd());
+
+my $login =  getlogin || getpwuid($<) || $ENV{USER} || "sundaramj";
+
+use constant DEFAULT_OUTDIR => '/tmp/' . $login . '/' . File::Basename::basename($0) . '/' . time();
+
+$|=1; ## do not buffer output stream
+
+## Command-line arguments
+my (
+    $indir, 
+    $outdir,
+    $config_file,
+    $log_level, 
+    $help, 
+    $logfile, 
+    $man, 
+    $verbose,
+    );
+
+my $results = GetOptions (
+    'log-level|d=s'                  => \$log_level, 
+    'logfile=s'                      => \$logfile,
+    'config_file=s'                  => \$config_file,
+    'help|h'                         => \$help,
+    'man|m'                          => \$man,
+    'indir=s'                        => \$indir,
+    'outdir=s'                       => \$outdir,
+    );
+
+&checkCommandLineArguments();
+
+my $logger = new DevelopmentUtils::Logger(
+    logfile   => $logfile, 
+    log_level => $log_level
+    );
+
+if (!defined($logger)){
+    die "Could not instantiate DevelopmentUtils::Logger";
+}
+
+my $config_manager = DevelopmentUtils::Config::Manager::getInstance(config_file => $config_file);
+if (!defined($config_manager)){
+    $logger->logdie("Could not instantiate DevelopmentUtils::Config::Manager");
+}
+
+my $helper = DevelopmentUtils::Perlbrew::Helper::getInstance(
+    indir  => $indir,
+    outdir => $outdir
+    );
+
+if (!defined($helper)){
+    $logger->logdie("Could not instantiate DevelopmentUtils::Perlbrew::Helper");
+}
+
+$helper->run();
+
+
+printGreen(File::Spec->rel2abs($0) . " execution completed\n");
+
+print "The log file is '$logfile'\n";
+
+exit(0);
+
+##-----------------------------------------------------------
+##
+##    END OF MAIN -- SUBROUTINES FOLLOW
+##
+##-----------------------------------------------------------
+
+sub checkCommandLineArguments {
+   
+    if ($man){
+    	&pod2usage({-exitval => 1, -verbose => 2, -output => \*STDOUT});
+    }
+    
+    if ($help){
+    	&pod2usage({-exitval => 1, -verbose => 1, -output => \*STDOUT});
+    }
+
+    if (!defined($config_file)){
+
+        $config_file = DEFAULT_CONFIG_FILE;
+            
+        printYellow("--config_file was not specified and therefore was set to default '$config_file'");
+    }
+
+    &checkInfileStatus($config_file);
+
+    if (!defined($verbose)){
+
+        $verbose = DEFAULT_VERBOSE;
+
+        printYellow("--verbose was not specified and therefore was set to default '$verbose'");
+    }
+
+    if (!defined($log_level)){
+
+        $log_level = DEFAULT_LOG_LEVEL;
+
+        printYellow("--log_level was not specified and therefore was set to default '$log_level'");
+    }
+
+    if (!defined($indir)){
+
+        $indir = DEFAULT_INDIR;
+
+        printYellow("--indir was not specified and therefore was set to default '$indir'");
+    }
+
+    $indir = File::Spec->rel2abs($indir);
+
+    if (!defined($outdir)){
+
+        $outdir = DEFAULT_OUTDIR;
+
+        printYellow("--outdir was not specified and therefore was set to default '$outdir'");
+    }
+
+    $outdir = File::Spec->rel2abs($outdir);
+
+    if (!-e $outdir){
+
+        mkpath ($outdir) || die "Could not create output directory '$outdir' : $!";
+
+        printYellow("Created output directory '$outdir'");
+
+    }
+    
+    if (!defined($logfile)){
+
+    	$logfile = $outdir . '/' . File::Basename::basename($0) . '.log';
+
+    	printYellow("--logfile was not specified and therefore was set to '$logfile'");
+
+    }
+
+    $logfile = File::Spec->rel2abs($logfile);
+
+    my $fatalCtr=0;
+
+    if ($fatalCtr> 0 ){
+    	die "Required command-line arguments were not specified\n";
+    }
+}
+
+sub printBoldRed {
+
+    my ($msg) = @_;
+    print color 'bold red';
+    print $msg . "\n";
+    print color 'reset';
+}
+
+sub printYellow {
+
+    my ($msg) = @_;
+    print color 'yellow';
+    print $msg . "\n";
+    print color 'reset';
+}
+
+sub printGreen {
+
+    my ($msg) = @_;
+    print color 'green';
+    print $msg . "\n";
+    print color 'reset';
+}
+
+
+sub checkOutdirStatus {
+
+    my ($outdir) = @_;
+
+    if (!-e $outdir){
+        
+        mkpath($outdir) || die "Could not create output directory '$outdir' : $!";
+        
+        printYellow("Created output directory '$outdir'");
+    }
+    
+    if (!-d $outdir){
+
+        printBoldRed("'$outdir' is not a regular directory\n");
+    }
+}
+
+
+sub checkInfileStatus {
+
+    my ($infile) = @_;
+
+    if (!defined($infile)){
+        die ("infile was not defined");
+    }
+
+    my $errorCtr = 0 ;
+
+    if (!-e $infile){
+
+        printBoldRed("input file '$infile' does not exist");
+
+        $errorCtr++;
+    }
+    else {
+
+        if (!-f $infile){
+
+            printBoldRed("'$infile' is not a regular file");
+
+            $errorCtr++;
+        }
+
+        if (!-r $infile){
+
+            printBoldRed("input file '$infile' does not have read permissions");
+
+            $errorCtr++;
+        }
+        
+        if (!-s $infile){
+
+            printBoldRed("input file '$infile' does not have any content");
+
+            $errorCtr++;
+        }
+    }
+     
+    if ($errorCtr > 0){
+
+        printBoldRed("Encountered issues with input file '$infile'");
+
+        exit(1);
+    }
+}
+
+
+
+
+__END__
+
+=head1 NAME
+
+ perlbrew_helper.pl - Perl script to help with executing perlbrew commands
+
+
+=head1 SYNOPSIS
+
+ perl util/perlbrew_helper.pl
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--indir>
+
+
+=item B<--outdir>
+
+
+=back
+
+=head1 DESCRIPTION
+
+ A script for helping with executing perlbrew commands.
+
+=head1 CONTACT
+
+ Jaideep Sundaram 
+
+ Copyright Jaideep Sundaram
+
+ Can be distributed under GNU General Public License terms
+
+=cut
