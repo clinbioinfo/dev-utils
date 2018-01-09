9653e7685e1c44373fbcf5a18ba77a4e217c3c1e
Jaideep Sundaram <sundaram.medimmune@gmail.com>
2018-01-09 16:36:57 -0500

    Adds support to install tree

--- doc/configure_ubuntu.sh
+++ doc/configure_ubuntu.sh
@@ -19,6 +19,9 @@ sudo apt-get install emacs25 -y
 echo "About to install terminator"
 sudo apt-get install terminator -y
 
+echo "About to install tree"
+sudo apt-get install -y tree
+
 echo "About to install Sublime Text"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
