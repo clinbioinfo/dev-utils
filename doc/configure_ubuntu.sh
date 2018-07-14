0580d89dbc39bf72585cc7338908436cbd4ee0de
Jaideep Sundaram <sundaram.medimmune@gmail.com>
2018-07-14 16:27:48 -0400

    Adds support to suppress emacs welcome splash screen

--- doc/configure_ubuntu.sh
+++ doc/configure_ubuntu.sh
@@ -15,6 +15,9 @@ sudo apt-get install chromium-browser -y
 
 echo "About to install emacs25"
 sudo apt-get install emacs25 -y
+echo ";;disable splash screen and startup message" >> ~/.emacs
+echo "(setq inhibit-startup-message t)" >> ~/.emacs
+echo "(setq initial-scratch-message nil)" >> ~/.emacs
 
 echo "About to install terminator"
 sudo apt-get install terminator -y
