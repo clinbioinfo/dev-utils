c53f714b1893e3f9091a2b4f963ae5f14ca2fe71
Jaideep Sundaram <clinbioinfo@gmail.com>
2020-03-31 11:06:42 -0400

    ADDS reference

--- doc/install_golang.sh
+++ doc/install_golang.sh
@@ -3,3 +3,4 @@ echo "About to install golang"
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt update
 sudo apt install golang-go
+echo "Reference: https://github.com/golang/go/wiki/Ubuntu"
