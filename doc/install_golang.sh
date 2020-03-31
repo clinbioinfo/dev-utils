2110f4df0a573d7753212f798d476fc4de28d4bd
Jaideep Sundaram <clinbioinfo@gmail.com>
2020-03-31 11:42:42 -0400

    ADDS support for adding GOPATH to the environment and the ~/.zshrc

--- doc/install_golang.sh
+++ doc/install_golang.sh
@@ -4,3 +4,12 @@ sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt update
 sudo apt install golang-go
 echo "Reference: https://github.com/golang/go/wiki/Ubuntu"
+
+echo "Adding GOPATH to environment"
+export GOPATH="/home/jsundaram/go"
+export PATH=$PATH:$GOPATH/bin
+
+echo "Adding GOPATH to ~/.zshrc"
+echo "export GOPATH=\"/home/jsundaram/go\"" >> ~/.zshrc
+echo "export PATH=$PATH:$GOPATH/bin" >> ~/.zshrc
+ 
