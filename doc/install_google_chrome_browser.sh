10739e953770c543f3170f3e8ee58cd802f689f6
Jaideep Sundaram <clinbioinfo@gmail.com>
2020-03-31 10:00:56 -0400

    updates

---
+++ doc/install_google_chrome_browser.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "About to install Google Chrome browser"
+sudo apt install -y wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
