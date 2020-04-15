b09b2e9b63329952855250e0a88985f70f5b9b82
Jaideep Sundaram <clinbioinfo@gmail.com>
2020-04-15 14:52:57 -0400

    initial check-in; install docker-compose

---
+++ doc/install_docker-compose.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "About to install docker-compose"
+echo "Reference: https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-18-04"
+
+VERSION=1.25.5
+
+echo "About to execute: sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose"
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+echo "About to execute: sudo chmod +x /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "About to execute: docker-compose --version"
+docker-compose --version
+
+
