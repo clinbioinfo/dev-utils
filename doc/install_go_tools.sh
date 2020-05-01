5ae3806df58aadb81402b35ad9c6c323baf1a1f2
Jaideep Sundaram <clinbioinfo@gmail.com>
2020-05-01 13:57:03 -0400

    initial check-in for installing go tools

---
+++ doc/install_go_tools.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+echo "About to execute: printenv | grep GO"
+printenv | grep GO
+
+echo "About to execute: go get github.com/mdempsky/gocode"
+go get github.com/mdempsky/gocode
+
+echo "About to execute: go get github.com/mdempsky/gocode"
+go get github.com/mdempsky/gocode
+
+echo "About to execute: go get -v github.com/mdempsky/gocode"
+go get -v github.com/mdempsky/gocode
+
+echo "About to execute: go get -v github.com/uudashr/gopkgs/v2/cmd/gopkgs"
+go get -v github.com/uudashr/gopkgs/v2/cmd/gopkgs
+
+echo "About to execute: go get -v github.com/ramya-rao-a/go-outline"
+go get -v github.com/ramya-rao-a/go-outline
+
+echo "About to execute: go get -v github.com/acroca/go-symbols"
+go get -v github.com/acroca/go-symbols
+
+echo "About to execute: go get -v golang.org/x/tools/cmd/guru"
+go get -v golang.org/x/tools/cmd/guru
+
+echo "About to execute: go get -v golang.org/x/tools/cmd/gorename"
+go get -v golang.org/x/tools/cmd/gorename
+
+echo "About to execute: go get -v github.com/cweill/gotests/..."
+go get -v github.com/cweill/gotests/...
+
+echo "About to execute: go get -v github.com/fatih/gomodifytags"
+go get -v github.com/fatih/gomodifytags
+
+echo "About to execute: go get -v github.com/josharian/impl"
+go get -v github.com/josharian/impl
+
+echo "About to execute: go get -v github.com/davidrjenni/reftools/cmd/fillstruct"
+go get -v github.com/davidrjenni/reftools/cmd/fillstruct
+
+echo "About to execute: go get -v github.com/haya14busa/goplay/cmd/goplay"
+go get -v github.com/haya14busa/goplay/cmd/goplay
+
+echo "About to execute: go get -v github.com/godoctor/godoctor"
+go get -v github.com/godoctor/godoctor
+
+echo "About to execute: go get -v github.com/go-delve/delve/cmd/dlv"
+go get -v github.com/go-delve/delve/cmd/dlv
+
+echo "About to execute: go get -v -d github.com/stamblerre/gocode"
+go get -v -d github.com/stamblerre/gocode
+
+echo "About to execute: go get -v github.com/rogpeppe/godef"
+go get -v github.com/rogpeppe/godef
+
+echo "About to execute: go get -v github.com/sqs/goreturns"
+go get -v github.com/sqs/goreturns
+
+echo "About to execute: go get -v golang.org/x/lint/golint"
+go get -v golang.org/x/lint/golint
+
+echo "About to execute: go get -v github.com/sqs/goreturns"
+go get -v github.com/sqs/goreturns
+
+echo "About to execute: go get -v github.com/ramya-rao-a/go-outline"
+go get -v github.com/ramya-rao-a/go-outline
