645f557d04674a4c1b8d4f4c6a16a17da0d3df2f
Jaideep Sundaram <clinbioinfo@gmail.com>
2020-05-05 18:31:33 -0400

    initial check-in; support for installing cpanminus

---
+++ doc/install_cpanminus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Reference: https://www.howtoinstall.me/ubuntu/18-04/cpanminus/"
+
+echo "About to execute: sudo apt update"
+sudo apt update
+
+echo "About to execute: sudo apt install cpanminus"
+sudo apt install cpanminus
