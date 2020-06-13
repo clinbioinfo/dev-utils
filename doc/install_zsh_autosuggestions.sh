bba313e5b98382c3d750c6369afd41554246ccaf
Jaideep Sundaram <clinbioinfo@gmail.com>
2020-06-13 09:18:55 -0400

    updates/corrections

--- doc/install_zsh_autosuggestions.sh
+++ doc/install_zsh_autosuggestions.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "About to install zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-source ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+echo "source $ZSH_CUSTOM/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+echo "Add zsh-autosuggestions to the plugins() list in the $HOME/.zshrc file and then execute:"
+echo "source $HOME/.zshrc"
 echo "Reference: https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md"
--- doc/install_zsh_syntax_highlighting.sh
+++ doc/install_zsh_syntax_highlighting.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 echo "About to install zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "Add zsh-syntax-highlighting to the plugins section in the ~/.zshrc"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+echo "source $ZSH_CUSTOM/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
+echo "Add zsh-syntax-highlighting to the plugins() list in the $HOME/.zshrc file and then execute:"
+echo "source $HOME/.zshrc"
+echo "Reference: https://gist.github.com/dogrocker/1efb8fd9427779c827058f873b94df95"
