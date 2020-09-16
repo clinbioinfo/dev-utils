5ddebc8ea2313e268bc3d043014e08d4b868ea24
Jaideep Sundaram <jaideep.sundaram@nih.gov>
2020-09-16 10:31:56 -0400

    ADDS export to ~/.zshrc

--- doc/install_oh_my_zsh.sh
+++ doc/install_oh_my_zsh.sh
@@ -4,3 +4,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 echo "Going to append the dev-utils/doc/aliases.txt to ~/.zshrc"
 echo "source ~/dev-utils/doc/aliases.txt" >> ~/.zshrc
+echo "export ZSH_CUSTOM=$HOME/.oh-my-zsh" >> $HOME/.zshrc
