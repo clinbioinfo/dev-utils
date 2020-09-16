e33d7414651019f0787820bd65bf5b9e8e614b27
Jaideep Sundaram <jaideep.sundaram@nih.gov>
2020-09-16 10:32:48 -0400

    ADDS source command

--- doc/install_zsh_syntax_highlighting.sh
+++ doc/install_zsh_syntax_highlighting.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "About to install zsh-syntax-highlighting"
+source $HOME/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 echo "source $ZSH_CUSTOM/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
 echo "Add zsh-syntax-highlighting to the plugins() list in the $HOME/.zshrc file and then execute:"
