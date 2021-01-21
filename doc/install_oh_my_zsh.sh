fe19ee8dd83392ed7413cbe287e7897909d13c44
Jaideep Sundaram <sundaram.medimmune@gmail.com>
2021-01-21 15:58:27 -0500

    ADDS support for installing zsh and curl

--- doc/install_oh_my_zsh.sh
+++ doc/install_oh_my_zsh.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+echo "Going to install zsh"
+sudo apt-get -y install zsh
+
+echo "Going to install curl"
+sudo apt-get install -y curl
+
 echo "Going to install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
