16e55b58e295f688437fdef8a5c3e6727a5a5f36
Jaideep Sundaram <clinbioinfo@gmail.com>
2021-03-05 08:28:13 -0500

    REMOVES post installation configuration steps because oh-my-zsh automatically changes the shell to zsh and thus the post installation steps never get executed.

--- doc/install_oh_my_zsh.sh
+++ doc/install_oh_my_zsh.sh
@@ -7,7 +7,3 @@ sudo apt-get install -y curl
 
 echo "Going to install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "Going to append the dev-utils/doc/aliases.txt to ~/.zshrc"
-echo "source ~/dev-utils/doc/aliases.txt" >> ~/.zshrc
-echo "export ZSH_CUSTOM=$HOME/.oh-my-zsh" >> $HOME/.zshrc
