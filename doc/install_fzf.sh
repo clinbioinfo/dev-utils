5cb14c01e51e83652f8b50210fa23f355798fef7
Jaideep Sundaram <sundaram.medimmune@gmail.com>
2023-04-03 21:30:48 -0400

    MOVES project into ~/.tools

--- doc/install_fzf.sh
+++ doc/install_fzf.sh
@@ -9,12 +9,12 @@ echo "Will attempt to execute: ~/.fzf/install"
 
 if [ -f ${HOME}/.zshrc ]; then
   echo "Will attempt to append exports in fzf_exports.sh to ${HOME}/.zshrc"
-  cat ${HOME}/dev-utils/doc/fzf_exports.sh >> ${HOME}/.zshrc
+  cat ${HOME}/.tools/dev-utils/doc/fzf_exports.sh >> ${HOME}/.zshrc
 fi
 
 if [ -f ${HOME}/.bashrc ]; then
   echo "Will attempt to append exports in fzf_exports.sh to ${HOME}/.bashrc"
-  cat ${HOME}/dev-utils/doc/fzf_exports.sh >> ${HOME}/.bashrc
+  cat ${HOME}/.tools/dev-utils/doc/fzf_exports.sh >> ${HOME}/.bashrc
 fi
 
 echo "Will attempt to execute: sudo apt install fd-find"
--- doc/install_oh_my_zsh_part2.sh
+++ doc/install_oh_my_zsh_part2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "Going to append the dev-utils/doc/aliases.txt to ~/.zshrc"
-echo "source ~/dev-utils/doc/aliases.txt" >> ~/.zshrc
+echo "Going to append the .tools/dev-utils/doc/aliases.txt to ~/.zshrc"
+echo "source ~/.tools/dev-utils/doc/aliases.txt" >> ~/.zshrc
 echo "export ZSH_CUSTOM=$HOME/.oh-my-zsh" >> $HOME/.zshrc
 echo "You need to execute:"
 echo "source ${HOME}/.zshrc"
